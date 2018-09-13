73a6bf6650e991cdce36c55954c4e5e11572a976
msommerfe <marco.sommerfeld@t.online.de>
2018-09-13 22:36:32 +0200

    function ReadFileFast works, also the function complete and pollutantmean

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,7 +1,11 @@
+
+#setwd("C:/devsbb/Git/datasciencecoursera")
+#source("pollutantmean.R")
+
 pollutantmean<- function(directory, pollutant, id){
       data<-NULL
       selectedPollutantValues<-NULL
-      allFilestoRead<- getFilesToRead(directory,id)
+      allFilestoRead<- getFilesToReadFast(directory,id)
 
             for (file in allFilestoRead){
             values <<- read.csv(file, header = TRUE)
@@ -22,20 +26,85 @@ pollutantmean<- function(directory, pollutant, id){
       myMean
 }
 
+complete<- function(directory, id){
+      allFilestoRead<- getFilesToReadFast(directory,id)
+      columnId<-NULL
+      columnNobs<- NULL
+      
+      for (file in allFilestoRead){
+            values <<- read.csv(file, header = TRUE)
+            if (is.null(columnId)){
+                  Zeile <- read.csv(file,nrows = 1)
+                  columnId<- rbind(Zeile[4])
+                  columnNobs<- rbind(table(complete.cases(values))["TRUE"])
 
-getFilesToRead<- function(directory, id){
-      filesToRead<-NULL
-      allFiles<- list.files(directory, full.names = TRUE)
-      for (file in allFiles){
-            Zeile <- read.csv(file,nrows = 1)
-            if (Zeile[4] %in% id){
-                  if (is.null(filesToRead))
-                        filesToRead<- file
-                  else{
-                        filesToRead<- c(filesToRead, file)
-                  }
+            }
+            else{
+                  Zeile <- read.csv(file,nrows = 1)
+                  columnId<- rbind(columnId, Zeile[4]) 
+                  columnNobs<- rbind(columnNobs,table(complete.cases(values))["TRUE"])
+                  
             }
       }
+      
+      myComplete<-data.frame(columnId,columnNobs)
+      colnames(myComplete)<-c("id","nobs")
+      
+      myComplete
+}
+      
+
+getFilesToReadFast<-function(directory,ids){
+
+      myIndex <- 0
+      
+      filesToRead <- vector(length = length(ids))
+      vId         <- c(length = length(allFiles))
+      vFile       <- c(length = length(allFiles))
+      
+      allFiles    <- list.files(directory, full.names = TRUE)
+      
+      for (file in allFiles){
+            myIndex<- myIndex+1
+            zeile <- read.csv(file,nrows = 1)
+            vFile[myIndex]<- file
+            vId[myIndex]<- as.integer(zeile[4])
+      }
+
+      mappingList <-list(vId,vFile)
+
+      myIndex<-0
+      for (i in ids){
+            myIndex<-myIndex+1
+            indexFile<- match(i,mappingList[[1]])
+            filesToRead[myIndex]<-mappingList[[2]][indexFile]
+            }
+
+      filesToRead
+      }
+
+
+getFilesToRead<- function(directory, id){
+      filesToRead <- c(length = length(id))
+      allFiles <- list.files(directory, full.names = TRUE)
+      fileIds  <- c(length = length(allFiles))
+      fileNames <- c(length = length(allFiles))
+      for (file in allFiles){
+            Zeile <- read.csv(file,nrows = 1)
+            fileIds<- c(fileIds, as.integer(Zeile[4]))
+
+            fileNames<- c(fileNames, file)
+      }
+      
+      for (i in id){
+            index<- match(i,fileIds)
+            print(index)
+            print(fileNames[index])
+            filesToRead<-c(filesToRead,fileNames[index])
+      }
+
+      #      print(paste("filestoRead",filesToRead))
+
       filesToRead
 }
 
